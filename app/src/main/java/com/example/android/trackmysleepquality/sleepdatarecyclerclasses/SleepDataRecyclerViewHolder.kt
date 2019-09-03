d637c2a006802ebc01904f37108fbcdb7833aaa8
shicks1080 <stuart.hicks@gmail.com>
2019-09-03 15:06:51 +0200

    Refactoring the Adapter
    Moving code associated with the ViewHolder into the ViewHodler class

--- app/src/main/java/com/example/android/trackmysleepquality/sleepdatarecyclerclasses/SleepDataRecyclerViewHolder.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepdatarecyclerclasses/SleepDataRecyclerViewHolder.kt
@@ -1,14 +1,43 @@
 package com.example.android.trackmysleepquality.sleepdatarecyclerclasses
 
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
 
 class SleepDataRecyclerViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
 
     val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
     val sleepQuality: TextView = itemView.findViewById(R.id.sleep_quality)
     val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
+    fun bindSleepViewHolderData(item: SleepNight) {
+        val res = itemView.context.resources
+
+        if (item.sleepQuality <= 1) {
+            sleepQuality.setTextColor(Color.RED)
+        } else {
+            //reset the colour when the view is reused
+            sleepQuality.setTextColor(Color.BLACK)
+        }
+        sleepQuality.text = convertNumericQualityToString(item.sleepQuality, res)
+        sleepLength.text = convertDurationToFormatted(item.startTimeMillis, item.endTimeMillis, res)
+
+        qualityImage.setImageResource(when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+
+
 }
