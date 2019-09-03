74a8cd3f4c191b49448c6f29cbed423e780a81c9
shicks1080 <stuart.hicks@gmail.com>
2019-09-03 15:05:04 +0200

    Refactoring the Adapter
    Moving code associated with the ViewHolder into the ViewHolder class.

--- app/src/main/java/com/example/android/trackmysleepquality/sleepdatarecyclerclasses/SleepDataRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepdatarecyclerclasses/SleepDataRecyclerViewAdapter.kt
@@ -1,12 +1,9 @@
 package com.example.android.trackmysleepquality.sleepdatarecyclerclasses
 
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
 class SleepDataRecyclerViewAdapter : RecyclerView.Adapter<SleepDataRecyclerViewHolder>() {
@@ -22,28 +19,10 @@ class SleepDataRecyclerViewAdapter : RecyclerView.Adapter<SleepDataRecyclerViewH
 
     override fun onBindViewHolder(holder: SleepDataRecyclerViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        if (item.sleepQuality <= 1){
-            holder.sleepQuality.setTextColor(Color.RED)
-        } else {
-            //reset the colour when the view is reused
-            holder.sleepQuality.setTextColor(Color.BLACK)
-        }
-        holder.sleepQuality.text = convertNumericQualityToString(item.sleepQuality, res)
-        holder.sleepLength.text = convertDurationToFormatted(item.startTimeMillis, item.endTimeMillis, res)
-
-        holder.qualityImage.setImageResource(when (item.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+        holder.bindSleepViewHolderData(item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SleepDataRecyclerViewHolder {
+   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SleepDataRecyclerViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.recycler_view_cell, parent, false)
         return SleepDataRecyclerViewHolder(view)
