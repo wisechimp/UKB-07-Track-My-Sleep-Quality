862daf4664db2e463af1c8d07f2fd6ccb56ecae9
shicks1080 <stuart.hicks@gmail.com>
2019-09-03 14:45:03 +0200

    Customising the Recycler View cell
    Creating the layout
    Updating the ViewHolder to reflect the layout
    Updating the adapter to reflect the ViewHolder.

--- app/src/main/java/com/example/android/trackmysleepquality/sleepdatarecyclerclasses/SleepDataRecyclerViewHolder.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepdatarecyclerclasses/SleepDataRecyclerViewHolder.kt
@@ -1,6 +1,14 @@
 package com.example.android.trackmysleepquality.sleepdatarecyclerclasses
 
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.trackmysleepquality.R
 
-class SleepDataRecyclerViewHolder (val textView: TextView): RecyclerView.ViewHolder(textView)
+class SleepDataRecyclerViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
+    val sleepQuality: TextView = itemView.findViewById(R.id.sleep_quality)
+    val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+}
